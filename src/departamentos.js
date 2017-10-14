8afb1bd631cadf34c033f30adf11863ec8a47f41
Sharmyn28 <sharmynrodriguez@gmail.com>
2017-10-14 16:25:01 -0500

    departamento Apurimac

---
+++ src/departamentos.js
@@ -0,0 +1,157 @@
+const departamentos=[
+    {
+        departamento: "Amazonas",
+        provincias:
+        [
+            {
+                provincias:"Chachapollas",
+                altura: 55
+            },
+            {
+                provincias:"Bagua",
+                altura: 55
+            },
+            {
+                provincias: "Bongará",
+                altura: 55
+            },
+            {
+                provincias: "Condorcanqui",
+                altura: 55
+            },
+            {
+                provincias: "Luya",
+                altura: 55
+            },
+            {
+                provincias: "Rodríguez Mendoza",
+                altura: 55
+            },
+            {
+                provincias: "Utcubamba",
+                altura: 55
+            }
+        ]
+    },
+    {
+        departamento: "Áncash",
+        provincias:
+        [
+            {
+                provincias:"Huaraz",
+                altura: 55
+            },
+            {
+                provincias:"Aija",
+                altura: 55
+            },
+            {
+                provincias: "Antonio Raimondi",
+                altura: 55
+            },
+            {
+                provincias: "Asunción",
+                altura: 55
+            },
+            {
+                provincias: "Bolognesi",
+                altura: 55
+            },
+            {
+                provincias: "Carhuaz",
+                altura: 55
+            },
+            {
+                provincias: "Carlos Fermín Fitzcarrald",
+                altura: 55
+            },
+            {
+                provincias: "Casma",
+                altura: 55
+            },
+            {
+                provincias: "Corongo",
+                altura: 55
+            },
+            {
+                provincias: "Huari",
+                altura: 55
+            },
+            {
+                provincias: "Huarmey",
+                altura: 55
+            },
+            {
+                provincias: "Huaylas",
+                altura: 55
+            },
+            {
+                provincias: "Mariscal Luzuriaga",
+                altura: 55
+            },
+            {
+                provincias: "Ocros",
+                altura: 55
+            },
+            {
+                provincias: "Pallasca",
+                altura: 55
+            },
+            {
+                provincias: "Pomobamba",
+                altura: 55
+            },
+            {
+                provincias: "Recuay",
+                altura: 55
+            },
+            {
+                provincias: "Santa",
+                altura: 55
+            },
+            {
+                provincias: "Sihuas",
+                altura: 55
+            },
+            {
+                provincias: "Yungay",
+                altura: 55
+            },
+        ]
+    },
+    {
+        departamento: "Apurímac",
+        provincias:
+        [
+            {
+                provincias:"Abancay",
+                altura: 55
+            },
+            {
+                provincias:"Andahuaylas",
+                altura: 55
+            },
+            {
+                provincias: "Antabamba",
+                altura: 55
+            },
+            {
+                provincias: "Aymaraes",
+                altura: 55
+            },
+            {
+                provincias: "Cotabambas",
+                altura: 55
+            },
+            {
+                provincias: "Chincheros",
+                altura: 55
+            },
+            {
+                provincias: "Grau",
+                altura: 55
+            }
+        ]
+    },
+
+];
