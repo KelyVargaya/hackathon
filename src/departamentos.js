fc164441d5edd0ea352c033d7b8339d6643b79de
Sharmyn28 <sharmynrodriguez@gmail.com>
2017-10-15 06:51:42 -0500

    arequipa complete

--- src/departamentos.js
+++ src/departamentos.js
@@ -393,56 +393,424 @@ const departamentos = [
 		]
 	},
 	{
-		departamento: "Áncash",
+		departamento: "ÁNCASH",
 		provincias:
 		[
 			{
-				provincias: "Huaraz",
-				altura: 55
+				provincias: "HUARAZ",
+				distritos: [
+					{
+						distrito: "HUARAZ",
+						altura: 3607
+					},
+					{
+						distrito: "COCHABAMBA",
+						altura: 2918
+					},
+					{
+						distrito: "COLCABAMBA",
+						altura: 2371
+					},
+					{
+						distrito: "HUANCHAY",
+						altura: 2706
+					},
+					{
+						distrito: "INDEPENDENCIA",
+						altura: 3440
+					},
+					{
+						distrito: "JANGAS",
+						altura: 2734
+					},
+					{
+						distrito: "LA LIBERTAD",
+						altura: 3517
+					},
+					{
+						distrito: "OLLEROS",
+						altura: 3576
+					},
+					{
+						distrito: "PAMPAS GRANDE",
+						altura: 2827
+					},
+					{
+						distrito: "PARIACOTO",
+						altura: 2318
+					},
+					{
+						distrito: "PIRA",
+						altura: 3352
+					},
+					{
+						distrito: "TARICA",
+						altura: 3133
+					},
+				]
 			},
 			{
-				provincias: "Aija",
-				altura: 55
+				provincias: "AIJA",
+				distritos: [
+					{
+						distrito: "AIJA",
+						altura: 3690
+					},
+					{
+						distrito: "CORIS",
+						altura: 3014
+					},
+					{
+						distrito: "HUACLLAN",
+						altura: 3246
+					},
+					{
+						distrito: "LA MERCED",
+						altura: 3764
+					},
+					{
+						distrito: "SUCCHA",
+						altura: 3164
+					}
+				]
 			},
 			{
-				provincias: "Antonio Raimondi",
-				altura: 55
+				provincias: "ANTONIO RAYMONDI",
+				distritos: [
+					{
+						distrito: "AIJA",
+						altura: 3690
+					},
+					{
+						distrito: "CORIS",
+						altura: 3014
+					},
+					{
+						distrito: "CHACCHO",
+						altura: 3186
+					},
+					{
+						distrito: "CHINGAS",
+						altura: 3223
+					},
+					{
+						distrito: "LLAMELLIN",
+						altura: 3297
+					},
+					{
+						distrito: "MIRGAS",
+						altura: 3583
+					},
+					{
+						distrito: "SAN JUAN DE RONTOY",
+						altura: 3597
+					}
+				]
 			},
 			{
-				provincias: "Asunción",
-				altura: 55
+				provincias: "ASUNCIÓN",
+				distritos: [ 
+					{
+						distrito: "ACOCHACA",
+						altura: 3237
+					},
+					{
+						distrito: "CHACAS",
+						altura: 3434
+					}
+				]
 			},
 			{
-				provincias: "Bolognesi",
-				altura: 55
+				provincias: "BOLOGNESI",
+				distritos: [
+					{
+						distrito: "ABELARDO PARDO LEZAMETA",
+						altura: 2202
+					},
+					{
+						distrito: "ANTONIO RAYMONDI",
+						altura: 2254
+					},
+					{
+						distrito: "AQUIA",
+						altura: 4221
+					},
+					{
+						distrito: "CAJACAY",
+						altura: 3549
+					},
+					{
+						distrito: "CANIS",
+						altura: 2759
+					},
+					{
+						distrito: "CHIQUIAN",
+						altura: 3684
+					},
+					{
+						distrito: "COLQUIOC",
+						altura: 1176
+					},
+					{
+						distrito: "HUALLANCA",
+						altura: 4059
+					},
+					{
+						distrito: "HUASTA",
+						altura: 3973
+					},
+					{
+						distrito: "HUAYLLACAYAN",
+						altura: 2855
+					},
+					{
+						distrito: "LA PRIMAVERA",
+						altura: 3360
+					},
+					{
+						distrito: "MANGAS",
+						altura: 3326
+					},
+					{
+						distrito: "PACLLON",
+						altura: 3811
+					},
+					{
+						distrito: "SAN MIGUEL DE CORPANQUI",
+						altura: 3410
+					},
+					{
+						distrito: "TICLLOS",
+						altura: 3678
+					}
+				]
 			},
 			{
-				provincias: "Carhuaz",
-				altura: 55
+				provincias: "CARHUAZ",
+				distritos: [
+					{
+						distrito: "ACOPAMPA",
+						altura: 2855
+					},
+					{
+						distrito: "AMASHCA",
+						altura: 3125
+					},
+					{
+						distrito: "ANTA",
+						altura: 3183
+					},
+					{
+						distrito: "ATAQUERO",
+						altura: 3216
+					},
+					{
+						distrito: "CARHUAZ",
+						altura: 3207
+					},
+					{
+						distrito: "MARCARA",
+						altura: 3080
+					},
+					{
+						distrito: "PARIAHUANCA",
+						altura: 3005
+					},
+					{
+						distrito: "SAN MIGUEL DE ACO",
+						altura: 3236
+					},
+					{
+						distrito: "SHILLA",
+						altura: 3145
+					},
+					{
+						distrito: "TINCO",
+						altura: 2673
+					},
+					{
+						distrito: "YUNGAR",
+						altura: 3234
+					}
+				]
 			},
 			{
-				provincias: "Carlos Fermín Fitzcarrald",
-				altura: 55
+				provincias: "CARLOS FERMíN FITZCARRALD",
+				distritos: [
+					{
+						distrito: "SAN LUIS",
+						altura: 3247
+					},
+					{
+						distrito: "SAN NICOLAS",
+						altura: 3249
+					},
+					{
+						distrito: "YAUYA",
+						altura: 3378
+					}
+				]
 			},
 			{
-				provincias: "Casma",
-				altura: 55
+				provincias: "CASMA",
+				distritos: [
+					{
+						distrito: "BUENA VISTA ALTA",
+						altura: 417
+					},
+					{
+						distrito: "CASMA",
+						altura: 125
+					},
+					{
+						distrito: "COMANDANTE NOEL",
+						altura: 18
+					},
+					{
+						distrito: "YAUTAN",
+						altura: 857
+					}
+				]
 			},
 			{
-				provincias: "Corongo",
-				altura: 55
+				provincias: "CORONGO",
+				distritos: [
+					{
+						distrito: "ACO",
+						altura: 3120
+					},
+					{
+						distrito: "BAMBAS",
+						altura: 2343
+					},
+					{
+						distrito: "CORONGO",
+						altura: 3171
+					},
+					{
+						distrito: "CUSCA",
+						altura: 3425
+					},
+					{
+						distrito: "LA PAMPA",
+						altura: 2210
+					},
+					{
+						distrito: "YANAC",
+						altura: 2347
+					},
+					{
+						distrito: "YUPAN",
+						altura: 2521
+					}
+				]
 			},
 			{
-				provincias: "Huari",
-				altura: 55
+				provincias: "HUARI",
+				distritos: [
+					{
+						distrito: "ANRA",
+						altura: 3094
+					},
+					{
+						distrito: "CAJAY",
+						altura: 3393
+					},
+					{
+						distrito: "CHAVIN DE HUANTAR",
+						altura: 3549
+					},
+					{
+						distrito: "HUACACHI",
+						altura: 3259
+					},
+					{
+						distrito: "HUACHIS",
+						altura: 3516
+					},
+					{
+						distrito: "HUANTAR",
+						altura: 3299
+					},
+					{
+						distrito: "HUARI",
+						altura: 3399
+					},
+					{
+						distrito: "MASIN",
+						altura: 3208
+					},
+					{
+						distrito: "PAUCAS",
+						altura: 3257
+					},
+					{
+						distrito: "PONTO",
+						altura: 3347
+					},
+					{
+						distrito: "RAHUAPAMPA",
+						altura: 2691
+					},
+					{
+						distrito: "RAPAYAN",
+						altura: 3387
+					},
+					{
+						distrito: "SAN MARCOS",
+						altura: 3686
+					},
+					{
+						distrito: "SAN MARCOS",
+						altura: 3711
+					},
+					{
+						distrito: "UCO",
+						altura: 2880
+					}
+				]
 			},
 			{
 				provincias: "Huarmey",
-				altura: 55
+				distritos: [
+					{
+						distrito: "COCHAPETI",
+						altura: 2652
+					},
+					{
+						distrito: "CULEBRAS",
+						altura: 270
+					},
+					{
+						distrito: "HUARMEY",
+						altura: 190.5
+					},
+					{
+						distrito: "HUAYAN",
+						altura: 2291
+					},
+					{
+						distrito: "HUARMEY",
+						altura: 190.5
+					},					
+				]
 			},
 			{
 				provincias: "Huaylas",
-				altura: 55
+				distritos: [
+					{
+						distrito: "SAN LUIS",
+						altura: 3247
+					},
+					{
+						distrito: "SAN NICOLAS",
+						altura: 3249
+					},
+					{
+						distrito: "YAUYA",
+						altura: 3378
+					}
+				]
 			},
 			{
 				provincias: "Mariscal Luzuriaga",
---
+++ src/provincias.js
@@ -0,0 +1,935 @@
+const departamentos = [
+	{
+		departamento: "AMAZONAS",
+		provincias:
+		[
+			{
+				provincias: "CHACHAPOLLAS",
+				altura : 2671
+			},
+			{
+                provincias: "BAGUA",
+                altura : 785
+			},
+			{
+                provincias: "BONGARÁ",
+                altura : 2142
+			},
+			{
+                provincias: "CONDORCANQUI",
+                altura : 320
+			},
+			{
+                provincias: "LUYA",
+                altura : 2371
+			},
+			{
+                provincias: "RODRÍGUEZ DE MENDOZA",
+                altura : 1915
+				
+			},
+			{
+                provincias: "UTCUBAMBA",
+                altura : 1477
+			}
+		]
+	},
+	{
+		departamento: "ÁNCASH",
+		provincias:
+		[
+			{
+                provincias: "HUARAZ",
+                altura : 3153
+			},
+			{
+				provincias: "AIJA",
+				altura : 3363
+			},
+			{
+				provincias: "ANTONIO RAYMONDI",
+				altura : 3314
+			},
+			{
+				provincias: "ASUNCIÓN",
+				altura : 3329
+			},
+			{
+				provincias: "BOLOGNESI",
+				altura : 3588
+			},
+			{
+				provincias: "CARHUAZ",
+				altura : 3135
+			},
+			{
+				provincias: "CARLOS FERMíN FITZCARRALD",
+				altura : 1915
+			},
+			{
+				provincias: "CASMA",
+				altura : 184
+			},
+			{
+				provincias: "CORONGO",
+				altura : 2975
+			},
+			{
+				provincias: "HUARI",
+				altura : 3418
+			},
+			{
+				provincias: "HUARMEY",
+				altura : 1721
+			},
+			{
+				provincias: "HUAYLAS",
+				altura : 2830
+			},
+			{
+				provincias: "MARISCAL LUZURIAGA",
+				altura: 3181
+			},
+			{
+				provincias: "OCROS",
+				altura: 2692
+			},
+			{
+				provincias: "PALLASCA",
+				altura: 3111
+			},
+			{
+				provincias: "POMABAMBA",
+				altura: 3391
+			},
+			{
+				provincias: "RECUAY",
+				altura: 3475
+			},
+			{
+				provincias: "SANTA",
+				altura: 1329
+			},
+			{
+				provincias: "SIHUAS",
+				altura: 3313
+			},
+			{
+				provincias: "YUNGAY",
+				altura: 2963
+			},
+		]
+	},
+	{ 
+		departamento: "APURIMAC",
+		provincias:
+		[
+			{
+				provincias: "ABANCAY",
+				altura: 3050
+			},
+			{
+				provincias: "ANDAHUAYLAS",
+				altura: 3421
+			},
+			{
+				provincias: "ANTABAMBA",
+				altura: 4177
+			},
+			{
+				provincias: "AYMARAES",
+				altura: 3425
+            },
+            {
+				provincias: "CHINCHEROS",
+				altura: 3028
+			},
+			{
+				provincias: "COTABAMBAS",
+				altura: 3810
+			},
+			{
+				provincias: "GRAU",
+				altura: 3647
+			}
+		]
+	},
+	{
+		departamento: "AREQUIPA",
+		provincias:
+		[
+			{
+				provincias: "AREQUIPA",
+				altura: 2865
+			},
+			{
+				provincias: "CAMANÁ",
+				altura: 249
+			},
+			{
+				provincias: "CARAVELí",
+				altura: 1118
+			},
+			{
+				provincias: "CASTILLA",
+				altura: 3323
+			},
+			{
+				provincias: "CAYLLOMA",
+				altura: 4290
+			},
+			{
+				provincias: "CONDESUYOS",
+				altura: 3263
+			},
+			{
+				provincias: "ISLAY",
+				altura: 195
+			},
+			{
+				provincias: "LA UNIóN",
+				altura: 3622
+			}
+		]
+	},
+	{
+		departamento: "Ayacucho",
+		provincias:
+		[
+			{
+				provincias: "Huamanga",
+				altura: 55
+			},
+			{
+				provincias: "Cangallo",
+				altura: 55
+			},
+			{
+				provincias: "Huancasancos",
+				altura: 55
+			},
+			{
+				provincias: "Huanta",
+				altura: 55
+			},
+			{
+				provincias: "La Mar",
+				altura: 55
+			},
+			{
+				provincias: "Lucanas",
+				altura: 55
+			},
+			{
+				provincias: "Parinacochas",
+				altura: 55
+			},
+			{
+				provincias: "Paúcar del Sara Sara",
+				altura: 55
+			},
+			{
+				provincias: "Sucre",
+				altura: 55
+			},
+			{
+				provincias: "Víctor Fajardo",
+				altura: 55
+			},
+			{
+				provincias: "Vilcashuamán",
+				altura: 55
+			}
+		]
+	},
+	{
+		departamento: "Cajamarca",
+		provincias:
+		[
+			{
+				provincias: "Cajamarca",
+				altura: 55
+			},
+			{
+				provincias: "Cajabamba",
+				altura: 55
+			},
+			{
+				provincias: "Celendín",
+				altura: 55
+			},
+			{
+				provincias: "Chota",
+				altura: 55
+			},
+			{
+				provincias: "Contumazá",
+				altura: 55
+			},
+			{
+				provincias: "Cutervo",
+				altura: 55
+			},
+			{
+				provincias: "Hualgayoc",
+				altura: 55
+			},
+			{
+				provincias: "Jaén",
+				altura: 55
+			},
+			{
+				provincias: "San Ignacio",
+				altura: 55
+			},
+			{
+				provincias: "San Marcos",
+				altura: 55
+			},
+			{
+				provincias: "San Miguel",
+				altura: 55
+			},
+			{
+				provincias: "San Pablo",
+				altura: 55
+			},
+			{
+				provincias: "Santa Cruz",
+				altura: 55
+			},
+		]
+	},
+	{
+		departamento: "Callao",
+		provincias:
+		[
+			{
+				provincias: "Callao",
+				altura: 55
+			}
+		]
+	},
+	{
+		departamento: "Cusco",
+		provincias:
+		[
+			{
+				provincias: "Cusco",
+				altura: 55
+			},
+			{
+				provincias: "Acomayo",
+				altura: 55
+			},
+			{
+				provincias: "Anta",
+				altura: 55
+			},
+			{
+				provincias: "Calca",
+				altura: 55
+			},
+			{
+				provincias: "Canas",
+				altura: 55
+			},
+			{
+				provincias: "Canchis",
+				altura: 55
+			},
+			{
+				provincias: "Chumbivilcas",
+				altura: 55
+			},
+			{
+				provincias: "Espinar",
+				altura: 55
+			},
+			{
+				provincias: "La Convención",
+				altura: 55
+			},
+			{
+				provincias: "Paruro",
+				altura: 55
+			},
+			{
+				provincias: "Paucartambo",
+				altura: 55
+			},
+			{
+				provincias: "Quispicanchi",
+				altura: 55
+			},
+			{
+				provincias: "Urubamba",
+				altura: 55
+			}
+		]
+	},
+	{
+		departamento: "Huancavelica",
+		provincias:
+		[
+			{
+				provincias: "Huancavelica",
+				altura: 55
+			},
+			{
+				provincias: "Acobamba",
+				altura: 55
+			},
+			{
+				provincias: "Angaraes",
+				altura: 55
+			},
+			{
+				provincias: "Castrovirreyna",
+				altura: 55
+			},
+			{
+				provincias: "Churcampa",
+				altura: 55
+			},
+			{
+				provincias: "Huaytará",
+				altura: 55
+			},
+			{
+				provincias: "Tayacaja",
+				altura: 55
+			}
+		]
+	},
+	{
+		departamento: "Huánuco",
+		provincias:
+		[
+			{
+				provincias: "Huánuco",
+				altura: 55
+			},
+			{
+				provincias: "Ambo",
+				altura: 55
+			},
+			{
+				provincias: "Dos de Mayo",
+				altura: 55
+			},
+			{
+				provincias: "Huacaybamba",
+				altura: 55
+			},
+			{
+				provincias: "Huamalíes",
+				altura: 55
+			},
+			{
+				provincias: "Leoncio Prado",
+				altura: 55
+			},
+			{
+				provincias: "Marañón",
+				altura: 55
+			},
+			{
+				provincias: "Pachitea",
+				altura: 55
+			},
+			{
+				provincias: "Puerto Inca",
+				altura: 55
+			},
+			{
+				provincias: "Lauricocha",
+				altura: 55
+			},
+			{
+				provincias: "Yariwilca",
+				altura: 55
+			}
+		]
+	},
+	{
+		departamento: "Ica",
+		provincias:
+		[
+			{
+				provincias: "Ica",
+				altura: 55
+			},
+			{
+				provincias: "Chincha",
+				altura: 55
+			},
+			{
+				provincias: "Nazca",
+				altura: 55
+			},
+			{
+				provincias: "Palpa",
+				altura: 55
+			},
+			{
+				provincias: "Pisco",
+				altura: 55
+			}
+		]
+	},
+	{
+		departamento: "Junín",
+		provincias:
+		[
+			{
+				provincias: "Huancayo",
+				altura: 55
+			},
+			{
+				provincias: "Concepción",
+				altura: 55
+			},
+			{
+				provincias: "Chanchamayo",
+				altura: 55
+			},
+			{
+				provincias: "Jauja",
+				altura: 55
+			},
+			{
+				provincias: "Junín",
+				altura: 55
+			},
+			{
+				provincias: "Satipo",
+				altura: 55
+			},
+			{
+				provincias: "Tarma",
+				altura: 55
+			},
+			{
+				provincias: "Yauli",
+				altura: 55
+			},
+			{
+				provincias: "Chupaca",
+				altura: 55
+			},
+		]
+	},
+	{
+		departamento: "La Libertad",
+		provincias:
+		[
+			{
+				provincias: "Trujillo",
+				altura: 55
+			},
+			{
+				provincias: "Ascope",
+				altura: 55
+			},
+			{
+				provincias: "Bolívar",
+				altura: 55
+			},
+			{
+				provincias: "Chepén",
+				altura: 55
+			},
+			{
+				provincias: "Julcán",
+				altura: 55
+			},
+			{
+				provincias: "Otuzco",
+				altura: 55
+			},
+			{
+				provincias: "Pacasmayo",
+				altura: 55
+			},
+			{
+				provincias: "Pataz",
+				altura: 55
+			},
+			{
+				provincias: "Santiago de Chuco",
+				altura: 55
+			},
+			{
+				provincias: "Gran Chimú",
+				altura: 55
+			},
+			{
+				provincias: "Virú",
+				altura: 55
+			}
+		]
+	},
+	{
+		departamento: "Lambayeque",
+		provincias:
+		[
+			{
+				provincias: "Chiclayo",
+				altura: 55
+			},
+			{
+				provincias: "Ferreñafe",
+				altura: 55
+			},
+			{
+				provincias: "Lambayeque",
+				altura: 55
+			}
+		]
+	},
+	{
+		departamento: "Lima",
+		provincias:
+		[
+			{
+				provincias: "Lima",
+				altura: 55
+			},
+			{
+				provincias: "Barranca",
+				altura: 55
+			},
+			{
+				provincias: "Cajatambo",
+				altura: 55
+			},
+			{
+				provincias: "Canta",
+				altura: 55
+			},
+			{
+				provincias: "Cañete",
+				altura: 55
+			},
+			{
+				provincias: "Huaral",
+				altura: 55
+			},
+			{
+				provincias: "Huarochirí",
+				altura: 55
+			},
+			{
+				provincias: "Huaura",
+				altura: 55
+			},
+			{
+				provincias: "Oyón",
+				altura: 55
+			}, {
+				provincias: "Yauyos",
+				altura: 55
+			}
+		]
+	},
+	{
+		departamento: "Loreto",
+		provincias:
+		[
+			{
+				provincias: "Maynas",
+				altura: 55
+			},
+			{
+				provincias: "Alto Amazonas",
+				altura: 55
+			},
+			{
+				provincias: "Loreto",
+				altura: 55
+			},
+			{
+				provincias: "Mariscal Ramón Castilla",
+				altura: 55
+			},
+			{
+				provincias: "Requena",
+				altura: 55
+			},
+			{
+				provincias: "Ucayali",
+				altura: 55
+			},
+			{
+				provincias: "Datem del Marañón",
+				altura: 55
+			}, {
+				provincias: "Putumayo",
+				altura: 55
+			}
+		]
+	},
+	{
+		departamento: "Madre de Dios",
+		provincias:
+		[
+			{
+				provincias: "Tambopata",
+				altura: 55
+			},
+			{
+				provincias: "Manu",
+				altura: 55
+			},
+			{
+				provincias: "Tahuamanu",
+				altura: 55
+			}
+		]
+	},
+	{
+		departamento: "Moquegua",
+		provincias:
+		[
+			{
+				provincias: "Marical Nieto",
+				altura: 55
+			},
+			{
+				provincias: "General Sánchez Cerro",
+				altura: 55
+			},
+			{
+				provincias: "Ilo",
+				altura: 55
+			}
+		]
+	},
+	{
+		departamento: "Pasco",
+		provincias:
+		[
+			{
+				provincias: "Pasco",
+				altura: 55
+			},
+			{
+				provincias: "Daniel Alcides Carrión",
+				altura: 55
+			},
+			{
+				provincias: "Oxapampa",
+				altura: 55
+			}
+		]
+	},
+	{
+		departamento: "Piura",
+		provincias:
+		[
+			{
+				provincias: "Piura",
+				altura: 55
+			},
+			{
+				provincias: "Ayabaca",
+				altura: 55
+			},
+			{
+				provincias: "Huancabamba",
+				altura: 55
+			},
+			{
+				provincias: "Morropón",
+				altura: 55
+			},
+			{
+				provincias: "Paita",
+				altura: 55
+			},
+			{
+				provincias: "Sullana",
+				altura: 55
+			},
+			{
+				provincias: "Talara",
+				altura: 55
+			},
+			{
+				provincias: "Sechura",
+				altura: 55
+			}
+		]
+	},
+	{
+		departamento: "Puno",
+		provincias:
+		[
+			{
+				provincias: "Puno",
+				altura: 55
+			},
+			{
+				provincias: "Azángaro",
+				altura: 55
+			},
+			{
+				provincias: "Carabaya",
+				altura: 55
+			},
+			{
+				provincias: "Chucuito",
+				altura: 55
+			},
+			{
+				provincias: "El Collao",
+				altura: 55
+			},
+			{
+				provincias: "Huancané",
+				altura: 55
+			},
+			{
+				provincias: "Lampa",
+				altura: 55
+			},
+			{
+				provincias: "Melgar",
+				altura: 55
+			},
+			{
+				provincias: "Moho",
+				altura: 55
+			},
+			{
+				provincias: "San Antonio de Putina",
+				altura: 55
+			},
+			{
+				provincias: "San Román",
+				altura: 55
+			},
+			{
+				provincias: "Sandia",
+				altura: 55
+			},
+			{
+				provincias: "Yunguyo",
+				altura: 55
+			}
+		]
+	},
+	{
+		departamento: "San Martín",
+		provincias:
+		[
+			{
+				provincias: "Moyobamba",
+				altura: 55
+			},
+			{
+				provincias: "Bellavista",
+				altura: 55
+			},
+			{
+				provincias: "El Dorado",
+				altura: 55
+			},
+			{
+				provincias: "Huallaga",
+				altura: 55
+			},
+			{
+				provincias: "Lamas",
+				altura: 55
+			},
+			{
+				provincias: "Mariscal Cáceres",
+				altura: 55
+			},
+			{
+				provincias: "Picota",
+				altura: 55
+			},
+			{
+				provincias: "Rioja",
+				altura: 55
+			},
+			{
+				provincias: "San Martín",
+				altura: 55
+			},
+			{
+				provincias: "Tocache",
+				altura: 55
+			}
+		]
+	},
+	{
+		departamento: "Tacna",
+		provincias:
+		[
+			{
+				provincias: "Tacna",
+				altura: 55
+			},
+			{
+				provincias: "Candarave",
+				altura: 55
+			},
+			{
+				provincias: "Provincia de Jorge Basadre",
+				altura: 55
+			},
+			{
+				provincias: "Tarata",
+				altura: 55
+			}
+		]
+	},
+	{
+		departamento: "Tumbes",
+		provincias:
+		[
+			{
+				provincias: "Tumbes",
+				altura: 55
+			},
+			{
+				provincias: "Contralmirante Villar",
+				altura: 55
+			},
+			{
+				provincias: "Zarumilla",
+				altura: 55
+			}
+		]
+	},
+	{
+		departamento: "Ucayali",
+		provincias:
+		[
+			{
+				provincias: "Coronel Portillo",
+				altura: 55
+			},
+			{
+				provincias: "Atalaya",
+				altura: 55
+			},
+			{
+				provincias: "Padre Abad",
+				altura: 55
+			},
+			{
+				provincias: "Purús",
+				altura: 55
+			}
+		]
+	},
+];
+
+console.log(departamentos);
+
+export default departamentos
