9a54cfa0230a560ea275c9668108f08ccecdca8d
Giovanna Oruro <orurog@gmail.com>
2017-10-16 17:08:52 -0500

    React Bootstrap

--- src/home.js
+++ src/home.js
@@ -1,73 +1,87 @@
 import React, { Component } from 'react';
 import './home.css';
-import {
-    NavLink
-} from 'react-router-dom'
-
+import { Form, FormGroup, FormControl, Col, Grid, Row, ControlLabel, Button, ButtonToolbar,Nav, Navbar, NavItem, } from 'react-bootstrap';
+import {NavLink} from 'react-router-dom'
 import logo from './img/logo.png'
-import fondo from './img/fondoo.jpg'
 import responsive from './img/responsive.png'
 import play from './img/play.png'
 import paso2 from './img/2.png'
 import paso1 from './img/1.png'
 
-const Home = () => {
-    return (
-        <div>
-            <section className="home cursiva">
-                <div className="container-fluid">
-                    <div className="row menu">
-                        <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4 text-left">
-                            <img className="logo img-responsive" src={logo} alt="" />
-                        </div>
+class HeaderApp extends Component{
+	render(){
+	return(
+	   <Navbar className="menu">
+            <Navbar.Header >
+                <Navbar.Brand className="brand">
+                    <img className="img-responsive" src={logo} alt="" />
+                </Navbar.Brand>
+                <Navbar.Toggle />
+            </Navbar.Header>
+            <Navbar.Collapse>
+                <Nav pullRight>
+                    <NavItem className="btnMapa" eventKey={1} href="#">¿Como Funciona?</NavItem>
+                    <NavItem className="btnMapa" eventKey={2} href="#">Ver Mapa</NavItem>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>);
+	}
+  }
 
-                        <div className="col-xs-8 col-sm-8 col-md-8 col-lg-8 text-right buto" >
-                            <a className="anchor" href="">¿Como Funciona?</a>
-                            <a className="btn btn-default anchor"
-							href='http://tabfacil.com/temporal/www.hacknemia.com/?page=mapa'>
-                                Ver Mapa
-                            </a>
-                        </div>
-                    </div>
-                </div>
+ class Start extends Component{
+    render(){
+        return(
+            <Grid fluid className="home">
+                <Row>
+                    <Col className="titulo" xs={12} md={12}><h1>Diagnóstico fácil, rápido y preciso de la anemia </h1></Col>
+                </Row>
+                <Row>
+                    <Col lg={6} xs={8} lgOffset={3} xsOffset={2}> 
+                         <NavLink to={"/form"}
+                             className="btn text-center login-enviar">Iniciar</NavLink>
+                    </Col>
+                </Row>
+            </Grid>
+        );
+    }
+ }
 
-                <div className="titulo col-md-12 col-sm-12 col-xs-12">
-                    <h1>Diagnóstico fácil, rápido y preciso de la anemia </h1>
-                </div>
-
-                <div className="row">
-                    <div className="col-md-6 col-md-offset-3 text-center">
-                        <NavLink to={"/form"}
-                             className="btn text-center login-enviar">
-                                Iniciar
-                        </NavLink>
-                    </div>
-                </div>
-            </section>
-
-            <section className="funcion cursiva">
-                <div className="funcionalidad col-md-12 col-sm-12 col-xs-12 text-center ">
-                    <p>¿CÓMO FUNCIONA?</p>
-                    <h1> Acceder a Hacknemia es muy fácil.</h1>
-                    <p>Puede hacerlo desde su consultorio o su movil. </p>
-                        <img className="img-responsive responsive" src={responsive} alt=""/>
-                        <div className="play">
-                            <p>En su movil o tableta, descargue Hacknemia en la tienda de aplicaciones Google Play Store.</p>
-                            <img  src={play}/>  
-                        </div>
-                        <div className="pasos">
-                            <h1>Pasos a seguir</h1>
-                            <p>Paso 1 : Llenar el fomulario con datos del Niño</p>
-                            <img src={paso2} alt=""/><hr/><hr/>
-
-                            <p>Paso 2 : De acuerdo a los datos ingresados te retornara el resultado y recomendaciones</p>
-                            <img src={paso1} alt=""/>
-                        </div>
-                </div>
-            </section>
-         </div>
-    
-    );
+class Functionality extends Component{
+    render(){
+        return(
+            <Grid fluid className="functionality text-center">
+                <Row>
+                    <Col lg={12} xs={12}>
+                        <p>¿CÓMO FUNCIONA?</p>
+                        <h1> Acceder a Hacknemia es muy fácil.</h1>
+                        <p>Puede hacerlo desde su consultorio o su movil. </p>
+                        <img className="img-responsive" src={responsive} alt=""/>
+                        <hr/>
+                    </Col>
+                    <Col lg={12} xs={12} className="play">
+                        <p>En su movil o tableta, descargue Hacknemia en la tienda de aplicaciones Google Play Store.</p>
+                        <img  className="img-responsive" src={play}/> 
+                    </Col>
+                    <Col lg={12} xs={12} className="steps">
+                        <h1>Pasos a seguir</h1>
+                        <p>Paso 1 : Llenar el fomulario con datos del Niño</p>
+                        <img src={paso2} alt=""/><hr/><hr/>
+                        <p>Paso 2 : De acuerdo a los datos ingresados te retornara el resultado y recomendaciones</p>
+                        <img src={paso1} alt=""/>
+                    </Col>
+                </Row>
+            </Grid>
+        );
+    }
 }
-
+class Home extends Component {
+         render(){
+            return (<div> 
+                        <HeaderApp/>
+                        <Start/>
+                        <Functionality/>
+                    </div>   
+            );
+        }
+    }
 export default Home;
