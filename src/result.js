2dab1ec4e7a8913d0b1d5f9bc4678e3378dd550a
Sharmyn28 <sharmynrodriguez@gmail.com>
2017-10-18 12:13:29 -0500

    component results completo

--- src/result.js
+++ src/result.js
@@ -28,7 +28,7 @@ class HeaderApp extends Component {
 	}
 }
 
-const Result = ({ model }) => {
+const PrevPage = ({model}) => {
 	const reiniciar = () => {
 		Object.keys(model.info).filter(a => a != 'ageType').map(a => {
 			if (a === 'ageType') {
@@ -37,32 +37,42 @@ const Result = ({ model }) => {
 				model.info[a] = '';
 			}
 		})
-		/*	href='http://tabfacil.com/temporal/www.hacknemia.com/?page=mapa' */
 	}
-	const Diagnostic = () => {
-		return (
-			<Grid className='fomulario'>
-				<h1 style={{ color: 'black', marginTop: '0' }} className='text-center '>Resultado</h1>
-				<div className="form">
-					<div className="text-center contenido">
-						<p>CANTIDAD DE HEMOGLOBINA</p>
-						<div className="img-circle circle"><h2>{model.diagnost[0]}</h2></div>
-						<div className="text-center tipo"><h3>{model.diagnost[1].toUpperCase()}</h3></div>
-						<div className="receta text-center">
-							<h3 style={{ marginTop: '0' }}>Receta Médica</h3>
-							<p>{model.diagnost[2][0]}</p>
-							<p>{model.diagnost[2][1]}</p>
-							<p>{model.diagnost[2][2]}</p>
-						</div>
+	return (
+		<NavLink to="/form" style={{ display: 'flex', alignItems: 'center', marginLeft: '20px' }} onClick={reiniciar}>
+			<i className="material-icons">keyboard_arrow_left</i>
+			<span>Return</span>
+		</NavLink>
+	)
+}
+
+const Diagnostic = ({ model }) => {
+	return (
+		<Grid className='fomulario'>
+			<h1 style={{ color: 'black', marginTop: '0' }} className='text-center '>Resultado</h1>
+			<div className="form">
+				<div className="text-center contenido">
+					<p>CANTIDAD DE HEMOGLOBINA</p>
+					<div className="img-circle circle"><h2>{model.diagnost[0]}</h2></div>
+					<div className="text-center tipo"><h3>{model.diagnost[1].toUpperCase()}</h3></div>
+					<div className="receta text-center">
+						<h3 style={{ marginTop: '0' }}>Receta Médica</h3>
+						<p>{model.diagnost[2][0]}</p>
+						<p>{model.diagnost[2][1]}</p>
+						<p>{model.diagnost[2][2]}</p>
 					</div>
 				</div>
-			</Grid>
-		)
-	}
+			</div>
+		</Grid>
+	)
+}
+const Result = ({ model }) => {
+	/*	href='http://tabfacil.com/temporal/www.hacknemia.com/?page=mapa' */
 	return (
 		<div>
 			<HeaderApp />
-			<div>{Diagnostic()}</div>
+			<PrevPage model={model}/>
+			<Diagnostic model={model} />
 		</div>
 	);
 }
